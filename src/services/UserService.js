7ba41429efa1e5cb917ba615134438e2c48cd565
jkind <jose.kindelan@peoplewalking.com>
2023-01-27 07:26:03 -0500

    added config file

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,9 +1,4 @@
-const baseUrl = "https://gorest.co.in/public/v2/users";
-const token = "88f365ab9d194d3c264c5c6af27985e4139338e8dc0f6be47ff2a5fca5c15cc1";
-const headers = {
-    Accept: "application/json",
-        "Content-Type": "application/json"
-}
+import {baseUrl, headers, token} from "../utils/config";
 
 export const getUsers = ()=>{
     return fetch(
---
+++ src/utils/config.js
@@ -0,0 +1,6 @@
+export const baseUrl = "https://gorest.co.in/public/v2/users";
+export const token = "88f365ab9d194d3c264c5c6af27985e4139338e8dc0f6be47ff2a5fca5c15cc1";
+export const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+}
